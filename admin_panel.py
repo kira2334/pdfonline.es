958513772ec551268ef99ab2a1a51ce90be0d9f7
trabajo en local <jorgesuarez061020@gmail.com>
2022-07-25 09:09:49 -0500

    commit05

--- admin_panel.py
+++ admin_panel.py
@@ -5,3 +5,6 @@ admin_panel = Blueprint('admin_panel', __name__)
 @admin_panel.route("/admin/")
 def index_admin():
    return render_template("repos/admin_template/index.html")
+@admin_panel.route("/admin/<path:ruta>")
+def index_admin():
+   return render_template("repos/admin_template/"+ ruta)
